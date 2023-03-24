d3fd88e9e2704ffec5fa9616285938bbfb0a4a5b
Laisses <laisse@gmail.com>
2023-03-24 14:36:09 -0300

    add base colors, images and font to constants.js

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+import dummy from "../assets/dummy.jpg";
+import banner from "../assets/crafty-banner.png";
+import journal from "../assets/journal.jpg";
+import logo from "../assets/logo.png";
+import seal from "../assets/seal.jpg";
+import sewing from "../assets/sewing.jpg";
+import soap from "../assets/soap.jpg";
+import wood from "../assets/wood.jpg";
+
+export const IMAGES = { dummy, banner, journal, logo, seal, sewing, soap, wood };
+
+export const COLORS = {
+    TEXT: {
+        DARK_GREEN: "#3F4B49",
+        LIGHT_GREEN: "#4E8078",
+        ACCENT_GREEN: "17453E",
+    },
+    BUTTON: {
+        LIGHT_GREEN: "#4E8078",
+        LIGHT_PINK: "#CC9192",
+    },
+    BACKGROUND: {
+        LIGTH_GREY: "#FAFAFA",
+        ACCENT_GREY: "#F5F5F5",
+    },
+    FORM: {
+        LIGTH_GREY: "#EAEAEA",
+        DARK_GREY: "#696969",
+    },
+};
+
+export const PRIMARY_FONT = "font-family: 'Inter', sans-serif;";
