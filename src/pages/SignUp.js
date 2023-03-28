7c2be7927e40fbf7ec69e64f3f713f8f19d6391a
Laisses <laisse@gmail.com>
2023-03-28 09:42:45 -0300

    src/pages/SignUp.js: create sign-up page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import { COLORS, device } from "../constants";
-import { MainButton } from "../components/MainButton";
+import { SignUpForm } from "../components/SignUpForm";
 import dummy from "../assets/dummy.jpg";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
     return (
@@ -13,6 +15,15 @@ export const SignUp = () => {
                 />
             </LeftSide>
             <RightSide>
+                <Logo
+                    src={logo}
+                    alt="Crafty Inspire logo"
+                />
+                <Subtitle>Welcome to Crafty Inspire</Subtitle>
+                <SignUpForm />
+                <Paragraph>
+                    Already have an account? <Underline to="/sign-in">Sign-in</Underline>
+                </Paragraph>
             </RightSide>
         </Container>
     );
@@ -38,6 +49,9 @@ const LeftSide = styled.div`
 const RightSide = styled.div`
     height: 100vh;
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 const Image = styled.img`
@@ -49,4 +63,43 @@ const Image = styled.img`
         width: 250px;
         height: 370px;
     }
+`;
+
+const Logo = styled.img`
+    width: 190px;
+    height: 57px;
+
+    cursor: pointer;
+
+    margin-top: 20px;
+
+    @media ${device.tablet} {
+        width: 170px;
+        height: 52px;
+    }
+`;
+
+const Subtitle = styled.h1`
+    margin: 30px 0;
+
+    font-size: 24px;
+    font-weight: 300;
+    color: ${COLORS.FORM.DARK_GREY};
+
+    @media ${device.tablet} {
+        font-size: 18px;
+    }
+`;
+
+const Paragraph = styled.p`
+    font-size: 14px;
+    color: ${COLORS.FORM.DARK_GREY};
+    margin-top: 50px;
+`;
+
+const Underline = styled(Link)`
+    color: ${COLORS.FORM.DARK_GREY};
+    text-decoration: none;
+    border-bottom: 1px solid ${COLORS.TEXT.LIGHT_GREEN};
+    cursor: pointer;
 `;
