dc5332b6682890a0c164112f951c0b91b25fd395
Laisses <laisse@gmail.com>
2023-03-28 07:56:08 -0300

    src/styles/GlobalStyles.js: add font-family to Global Styles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import { PRIMARY_FONT } from "../constants";
 
 export const GlobalStyle = createGlobalStyle`
     /* http://meyerweb.com/eric/tools/css/reset/
@@ -37,6 +38,7 @@ footer, header, hgroup, menu, nav, section {
 }
 body {
 	line-height: 1;
+	font-family: ${PRIMARY_FONT};
 }
 ol, ul {
 	list-style: none;
