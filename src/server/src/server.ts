43b38042f7d029d5b57a5867025dcad677cd6fdd
Laisses <laisse@gmail.com>
2023-04-16 17:31:21 -0300

    src/server/src/server.ts: Integrate Apollo into the existing express application

--- src/server/src/server.ts
+++ src/server/src/server.ts
@@ -1,54 +1,44 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { routes } from "./routes";
+import { connection } from "./database";
+import * as migrations from "./migrations";
+import { db } from "./mock-data";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs } from "./schema";
 import { query } from "./resolvers/query";
 import { mutation } from "./resolvers/mutation";
-import { db } from "./mock-data";
-import dotenv from "dotenv";
 dotenv.config();
 
-const server = new ApolloServer({
+export const app = express();
+const PORT = Number(process.env.PORT) || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const apollo = new ApolloServer({
     typeDefs,
     resolvers: { ...query, ...mutation },
 });
 
-const PORT = Number(process.env.PORT) || 5000;
-
 const main = async () => {
-    const { url } = await startStandaloneServer(server, {
-        listen: {port: PORT},
+    routes(app);
+    await migrations.run(connection);
+
+    await apollo.start();
+    app.use("/graphql", expressMiddleware(apollo, {
         context: async () => {
             return {
                 db
             }
         }
-    });
-    console.log(`🚀 Server ready at: ${url}`);
-};
-main();
-
-
-/* import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { routes } from "./routes";
-import * as migrations from "./migrations";
-dotenv.config();
-
-export const app = express();
-const PORT = process.env.PORT;
-
-app.use(express.json());
-app.use(cors());
-
-const main = async () => {
-    routes(app);
-
-    await migrations.run();
+    }));
 
     app.listen(PORT, () => {
         console.log(`server running on port: ${PORT}`);
     });
 }
 
-main(); */
+main();
