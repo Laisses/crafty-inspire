9ae20f18acb7209439ddc89b511a9a8b04118f9f
Laisses <laisse@gmail.com>
2023-04-17 11:04:58 -0300

    src/server/src/handlers.ts: create the register new user handler

--- src/server/src/handlers.ts
+++ src/server/src/handlers.ts
@@ -1,5 +1,39 @@
 import { Request, Response } from "express";
+import { NewUser, UserLogin, User } from "./protocols";
+import * as r from "./repositories";
+import bcrypt from "bcrypt";
 
 export const health = (_req: Request, res: Response) => {
     res.status(200).send("OK");
+};
+
+const checkUserAvailability = async (email: string, username: String) => {
+    const checkEmail = await r.selectUserByEmail(email);
+    const checkUsername = await r.selectUserByUsername(username);
+
+    if (checkEmail.rows[0] || checkUsername.rows[0]) {
+        return false;
+    }
+
+    return true;
+};
+
+export const registerUser = async (req: Request, res: Response) => {
+    const user = req.body as NewUser;
+
+    const isUserAvailable = await checkUserAvailability(user.email, user.username);
+
+    if (!isUserAvailable) {
+        return res.status(409).send({message: "this email or username is already registered"})
+    }
+
+    const hashPassword = await bcrypt.hashSync(user.password, 10);
+
+    const newUser = {
+        ...user,
+        password: hashPassword,
+    };
+
+    await r.InsertNewUser(newUser);
+    return res.sendStatus(201);
 };
