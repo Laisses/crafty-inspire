ad6089615d0f2cd8e4789ddf50f79b8582c7a9e1
Laisses <laisse@gmail.com>
2023-04-18 13:48:29 -0300

    src/repositories.ts: Add queries to login endpoint

--- src/server/src/repositories.ts
+++ src/server/src/repositories.ts
@@ -14,3 +14,11 @@ export const selectUserByUsername = (username: String): Promise<QueryResult<User
 export const InsertNewUser = ({name, username, email, password}): Promise<QueryResult<NewUser>> => {
     return connection.query(`INSERT INTO users (id, name, username, email, password) VALUES ($1, $2, $3, $4, $5);`, [uuid(), name, username, email, password]);
 };
+
+export const selectSessionByUserId = (user_id: String): Promise<QueryResult<String>> => {
+    return connection.query(`SELECT token FROM sessions WHERE user_id=$1;`, [user_id]);
+};
+
+export const createSession = (user_id: String): Promise<QueryResult<String>> => {
+    return connection.query(`INSERT INTO sessions (id, user_id, token) VALUES ($1, $2, $3) RETURNING token;`, [uuid(), user_id, uuid()]);
+};
