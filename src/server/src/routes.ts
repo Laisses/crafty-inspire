8775ba5b6fa261f9510e733c0027bf6a46053c36
Laisses <laisse@gmail.com>
2023-04-18 13:47:58 -0300

    src/routes.ts: Create a login endpoint

--- src/server/src/routes.ts
+++ src/server/src/routes.ts
@@ -7,4 +7,5 @@ export const routes = (app: Express) => {
     app.get("/health", h.health);
 
     app.post("/register", m.validate(s.newUser), m.asyncError(h.registerUser));
+    app.post("/login", m.validate(s.user), m.asyncError(h.login));
 };
