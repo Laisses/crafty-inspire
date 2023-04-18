bb4158542447cde218b734847c3cf65992ebb529
Laisses <laisse@gmail.com>
2023-04-18 13:48:51 -0300

    src/handlers.ts: Add a login handler

--- src/server/src/handlers.ts
+++ src/server/src/handlers.ts
@@ -13,17 +13,22 @@ const checkUserAvailability = async (email: string, username: String) => {
 
     if (checkEmail.rows.length !== 0) {
         return {
+            available: false,
             conflictMessage: "user is alredy registered"
         }
     }
 
     if (checkUsername.rows.length !== 0) {
         return {
+            available: false,
             conflictMessage: "this username is already in use"
         }
     }
 
-    return {available: true};
+    return {
+        available: true,
+        conflictMessage: null
+    };
 };
 
 export const registerUser = async (req: Request, res: Response) => {
@@ -31,7 +36,7 @@ export const registerUser = async (req: Request, res: Response) => {
 
     const isUserAvailable = await checkUserAvailability(user.email, user.username);
 
-    if (isUserAvailable.conflictMessage) {
+    if (!isUserAvailable.available) {
         return res.status(409).send(isUserAvailable.conflictMessage)
     }
 
@@ -44,4 +49,54 @@ export const registerUser = async (req: Request, res: Response) => {
 
     await r.InsertNewUser(newUser);
     return res.sendStatus(201);
+};
+
+const validateUser = async (email: string, password: String) => {
+    const userExistis = await r.selectUserByEmail(email);
+    const user = userExistis.rows[0];
+    const validPassword = bcrypt.compareSync(password, user?.password || "");
+
+    if (userExistis.rows.length === 0 || !validPassword) {
+        return {
+            validation: false,
+            id: null,
+            unauthorizedMessage: "invalid username or password"
+        };
+    }
+
+    return {
+        validation: true,
+        id: user.id,
+        unauthorizedMessage: null
+    };
+};
+
+const checkForActiveSession = async (id: string) => {
+    const openSession = await r.selectSessionByUserId(id);
+
+    if (openSession.rows.length === 0) {
+        return false;
+    }
+
+    return true;
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body as UserLogin;
+
+    const validUser = await validateUser(email, password);
+
+    if (!validUser.validation) {
+        return res.status(401).send(validUser.unauthorizedMessage);
+    }
+
+    const activeSession = await checkForActiveSession(validUser.id);
+
+    if (activeSession) {
+        return res.status(401).send({message: "this account is already logged in"});
+    }
+
+    const token = await r.createSession(validUser.id);
+
+    res.status(200).send(token.rows[0]);
 };
