24839dd0498429c3294df69320401f646fea2be1
Laisses <laisse@gmail.com>
2023-04-18 15:03:42 -0300

    src/middlewares.ts: create a authentication middleware

--- src/server/src/middlewares.ts
+++ src/server/src/middlewares.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import {AuthenticatedRequest} from "./protocols";
 import { validator } from "./joi-schemas";
+import * as r from "./repositories";
 
 export const validate = schema => (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const {error} = validator(schema, payload);
+    const { error } = validator(schema, payload);
 
     if (error) {
         const errors = error.details.map((detail) => detail.message);
@@ -25,4 +27,23 @@ export const asyncError = handlerFn => async (req: Request, res: Response, next:
             message: "Internal Server Error"
         });
     }
+};
+
+export const authenticate = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    const userInfo = await r.selectUserByToken(token);
+
+    if (userInfo.rows.length === 0) {
+        return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    req.user = userInfo.rows[0];
+
+    return next();
 };
