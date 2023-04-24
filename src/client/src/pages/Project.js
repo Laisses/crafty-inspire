2727c0f9e6bafec6c5067ccc162f6690b043eec7
Laisses <laisse@gmail.com>
2023-04-24 16:57:20 -0300

    pages/Project: Connect with GraphQl
    Add the query useProject to display the requested project
    and add the delete mutation to child component
    ProjectDisplay.js

--- src/client/src/pages/Project.js
+++ src/client/src/pages/Project.js
@@ -2,14 +2,35 @@ import styled from "styled-components";
 import { device, COLORS } from "../constants";
 import { Navbar } from "../components/Navbar";
 import { ProjectDisplay } from "../components/ProjectDisplay";
+import { useParams } from "react-router-dom";
+import { useProject } from "../hooks";
 
 export const Project = () => {
+    const { id } = useParams();
+    const { data, loading, error } = useProject(id);
+
+    const Display = () => {
+        if (loading) {
+            return <Text>Loading...</Text>;
+        }
+
+        if (error) {
+            return <Text>Something went wrong, came back later!</Text>
+        }
+
+        const { project } = data;
+
+        return (
+            <ProjectDisplay project={project} />
+        );
+    };
+
     return (
         <>
             <Navbar />
             <Container>
                 <ProjectContainer>
-                    <ProjectDisplay />
+                    <Display />
                 </ProjectContainer>
             </Container>
         </>
@@ -18,6 +39,7 @@ export const Project = () => {
 
 const Container = styled.div`
     background-color: ${COLORS.BACKGROUND.LIGTH_GREY};
+    height: 100vh;
     padding-top: 20px;
     padding-bottom: 20px;
 
@@ -39,4 +61,8 @@ const ProjectContainer = styled.div`
     @media ${device.mobileL} {
         width: 100%;
     }
+`;
+
+const Text = styled.p`
+    color: ${COLORS.TEXT.DARK_GREEN};
 `;
